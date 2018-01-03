97240068af08a4aac16b22adf45e0d92c94297ee
izatech-jeyanth <34860118+izatech-jeyanth@users.noreply.github.com>
2018-01-03 19:39:27 +0530

    Angular file

---
+++ webapp/care9-distributor/js/app.js
@@ -0,0 +1,86 @@
+var app = angular.module('distributor', [ 'ngRoute' ]);
+
+
+app.config(function($routeProvider) {
+	$routeProvider
+
+	.when("/", {
+		templateUrl : "dashboard.html",
+		controller : "DashBoardController"
+	}).when("/dashboard", {
+		templateUrl : "dashboard.html",
+		controller : "DashBoardController"
+	}).when("/inventory", {
+		templateUrl : "inventory.html",
+		controller : "InventoryController"
+	}).when("/manage-request", {
+		templateUrl : "manage-request.html",
+		controller : "ManageRequestController"
+	}).when("/manufacture", {
+		templateUrl : "manufacturer.html",
+		controller : "ManufacturerController"
+	}).when("/pharmancies", {
+		templateUrl : "pharmacies.html",
+		controller : "PharmanciesController"
+	}).when("/send-request", {
+		templateUrl : "send-request.html",
+		controller : "SendRequestController"
+	}).when("/batchid", {
+		templateUrl : "batchid.html"
+		
+	}).when("/logout", {
+		templateUrl : "logout.html"
+		
+	});
+});
+
+app.controller('DashBoardController', [ '$scope', function($scope) {
+
+} ]);
+app.controller('InventoryController', ['$scope', function($scope) {
+	
+	$scope.showForm = false;
+	$scope.addNewproduct = function() {
+		$scope.showForm = true;
+	};
+	$scope.cancel = function() {
+		$scope.showForm = false;
+	};
+} ]);
+app.controller('ManageRequestController', [ '$scope', function($scope) {
+	$scope.showInformation = false;
+	$scope.showDetails = function() {
+		$scope.showInformation = true;
+	};
+	$scope.hideDetails = function() {
+		$scope.showInformation = false;
+	};
+} ]);
+app.controller('ManufacturerController', [ '$scope', function($scope) {
+	$scope.showForm = false;
+	$scope.addNewManufacturer = function() {
+		$scope.showForm = true;
+	};
+	$scope.cancel = function() {
+		$scope.showForm = false;
+	};
+} ]);
+app.controller('PharmanciesController', [ '$scope', function($scope) {
+	$scope.showForm = false;
+	$scope.addNewPharmancies = function() {
+		$scope.showForm = true;
+	};
+	$scope.cancel = function() {
+		$scope.showForm = false;
+	};
+} ]);
+app.controller('SendRequestController', [ '$scope', function($scope) {
+	$scope.showForm = false;
+	$scope.createOrder = function() {
+		$scope.showForm = true;
+	};
+	$scope.cancel = function() {
+		$scope.showForm = false;
+	};
+} ]);
+
